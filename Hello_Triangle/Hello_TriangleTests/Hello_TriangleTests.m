d003828d7b38d2f3830275e1235a5c81f1df45aa
hjw <573986558@qq.com>
2020-07-03 15:17:34 +0800

    hello triangle(pod)

---
+++ Hello_Triangle/Hello_TriangleTests/Hello_TriangleTests.m
@@ -0,0 +1,37 @@
+//
+//  Hello_TriangleTests.m
+//  Hello_TriangleTests
+//
+//  Created by neotv on 2020/7/3.
+//  Copyright © 2020 neotv. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hello_TriangleTests : XCTestCase
+
+@end
+
+@implementation Hello_TriangleTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Hello_Triangle/Pods/Target Support Files/Pods-Hello_Triangle-Hello_TriangleUITests/Pods-Hello_Triangle-Hello_TriangleUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Hello_Triangle_Hello_TriangleUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Hello_Triangle_Hello_TriangleUITests
+@end
